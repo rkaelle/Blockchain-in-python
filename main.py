ceb450750fe188030542be13869e6c242d983715
rkalesalad <rkaelle2@gmail.com>
2022-12-06 09:05:11 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,37 @@
-import hashlib
-import time
-
 class Block:
-    def __init__(self, data, previous_hash):
+    def __init__(self, data, previous_hash, validator_address):
         self.data = data
         self.timestamp = time.time()
         self.previous_hash = previous_hash
+        self.validator_address = validator_address
         self.hash = self.calculate_hash()
 
     def calculate_hash(self):
         # Calculate the hash of the block using the data, timestamp, and previous hash
-        block_data = (str(self.data) + str(self.timestamp) + str(self.previous_hash)).encode()
-        return hashlib.sha256(block_data).hexdigest()
+        ...
 
 class Blockchain:
     def __init__(self):
         self.blocks = []
+        self.validators = {}
+
+    def add_validator(self, address, tokens):
+        # Add a new validator to the chain
+        self.validators[address] = tokens
 
     def add_block(self, data):
-        # Add a new block to the chain
-        if len(self.blocks) == 0:
-            previous_hash = None
-        else:
-            previous_hash = self.blocks[-1].hash
-
-        new_block = Block(data, previous_hash)
-        self.blocks.append(new_block)
+        # Add a new block to the chain using the PoS algorithm
+        ...
 
     def verify_chain(self):
         # Verify the integrity of the chain by checking the hashes of the blocks
-        for i in range(1, len(self.blocks)):
-            previous_block = self.blocks[i-1]
-            current_block
+        ...
+
+blockchain = Blockchain()
+blockchain.add_validator("Validator1", 100)
+blockchain.add_validator("Validator2", 50)
+blockchain.add_validator("Validator3", 25)
+
+blockchain.add_block("This is the first block in the chain", "Validator1")
+blockchain.add_block("This is the second block in the chain", "Validator2")
+blockchain.add_block("This is the third block in the chain", "Validator3")
