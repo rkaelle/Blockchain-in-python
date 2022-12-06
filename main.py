fb83573caa9d32a5ef5b6d20d724dfe5ff7daf08
rkalesalad <rkaelle2@gmail.com>
2022-12-06 09:01:46 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import hashlib
+import time
+
 class Block:
     def __init__(self, data, previous_hash):
         self.data = data
@@ -7,7 +10,8 @@ class Block:
 
     def calculate_hash(self):
         # Calculate the hash of the block using the data, timestamp, and previous hash
-        ...
+        block_data = (str(self.data) + str(self.timestamp) + str(self.previous_hash)).encode()
+        return hashlib.sha256(block_data).hexdigest()
 
 class Blockchain:
     def __init__(self):
@@ -15,13 +19,16 @@ class Blockchain:
 
     def add_block(self, data):
         # Add a new block to the chain
-        ...
+        if len(self.blocks) == 0:
+            previous_hash = None
+        else:
+            previous_hash = self.blocks[-1].hash
+
+        new_block = Block(data, previous_hash)
+        self.blocks.append(new_block)
 
     def verify_chain(self):
         # Verify the integrity of the chain by checking the hashes of the blocks
-        ...
-
-blockchain = Blockchain()
-blockchain.add_block("This is the first block in the chain")
-blockchain.add_block("This is the second block in the chain")
-blockchain.add_block("This is the third block in the chain")
+        for i in range(1, len(self.blocks)):
+            previous_block = self.blocks[i-1]
+            current_block
